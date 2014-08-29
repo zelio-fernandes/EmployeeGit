cc8cde2b9290b99380efa9d610bc30d6f215f650
zelio <zelio@zelio-PC>
2014-08-29 14:23:22 +0530

    2nd checkin

---
+++ EmployeeGit/src/Employee.java
@@ -0,0 +1,29 @@
+
+public class Employee {
+
+	private int id;
+	private String employeeName;
+	private int salary;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	
+	
+	
+}
---
+++ EmployeeGit/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Employee e = new Employee();
+		e.setId(1);
+		
+		
+		System.out.println(e.getId());
+		System.out.println();
+		
+	}
+
+}
